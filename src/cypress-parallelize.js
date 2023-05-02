215478129558be14b2de98ce02216b22e651321a
Marco <gardosen@gmx.de>
2023-05-02 15:27:34 +0200

    updated version to 1.1.3 removed stream output code for log files (needs to be refactored)

--- src/cypress-parallelize.js
+++ src/cypress-parallelize.js
@@ -16,17 +16,18 @@ export async function executeCommandXTimes(args) {
     cleanFeatureFiles(); //clean the found feature files from previous runner annotations (only works with the default annotation or if the custom annotation is the same as before
     assignTestRunnerAnnotation();
 
-    if( !scope.options.dryRun ) {
-        for (let i = 1; i <= scope.runnerAmountAssigned; i++) {
-            try {
-                const { stdout } = execAsync(`npx cypress run --browser ${scope.options.browser} --config-file ${scope.options.configFile} --env allure=${scope.options.allure},tags="${scope.options.tags} and ${scope.options.runnerAnnotation}${i}"${scope.options.specFile != null? " --spec "+scope.options.specFile:""}`);
-                console.log(stdout);
-                //TODO get the pipes working to log the output to files
-                //const logStream = fs.createWriteStream(`log-${i}.txt`, { flags: 'a' });
-                //stdout.pipe(logStream);
-            } catch (error) {
-                console.error(`Iteration ${i + 1} - Error: ${error.message}`);
+    for (let i = 1; i <= scope.runnerAmountAssigned; i++) {
+        try {
+            const command = `npx cypress run --browser ${scope.options.browser} --config-file ${scope.options.configFile} --env allure=${scope.options.allure},tags="${scope.options.tags} and ${scope.options.runnerAnnotation}${i}"${scope.options.specFile != null? " --spec "+scope.options.specFile:""}`;
+            console.log(`Spawning console command [${command}]`)
+            if( !scope.options.dryRun ) {
+                const {stdout} = execAsync(command);
+                //TODO Think about printout to log files
             }
+
+        } catch (error) {
+            console.error(`Iteration ${i + 1} - Error: ${error.message}`);
         }
     }
+
 }
