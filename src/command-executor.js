55318cd30d1cee423c3f435b312ebdb78993cc68
Marco Bierbach <marco.bierbach@kriegerdigital.de>
2023-06-19 21:16:53 +0200

    experimental implementation of spawn to fix the buffer overflow in a pipeline with low resources

--- src/command-executor.js
+++ src/command-executor.js
@@ -1,55 +1,29 @@
 import fs from "fs";
 import path from "path";
-
-const { exec } = require( 'child_process' );
-const { promisify } = require( 'util' );
-const execAsync = promisify( exec );
+const spawn = require( 'child_process' ).spawn;
 const scope = require( './scope' );
 const logger = require( './logger' );
 
 
 const runnerList = [];
 
-export function executeCommandXTimes() {
-    for (let i = 1; i <= scope.runnerAmountAssigned; i++) {
-            const command = `npx cypress run --browser ${scope.options.browser} --config-file ${scope.options.configFile} --env ${scope.options.allure? `allure=${scope.options.allure},allureClearSkippedTests=true,allureResultsPath=${scope.options.allureRunnerReportFolderName}/${scope.options.runnerAnnotation}${i},`:''}tags="${scope.options.tags!==null? `${scope.options.tags} and `: '' }${scope.options.runnerAnnotation}${i}"${scope.options.spec != null? ` --spec ${scope.options.spec}`:""}`;
-            logger.log(`Spawning console command [${command}]`)
-            if( !scope.options.dryRun ) {
-                const promise = execAsync(command);
+export async function executeCommandXTimes(executionDetails) {
+    if( !scope.options.dryRun ) {
+        //new variant
+        const timeMap = new Map();
+        const promise = new Promise((resolve, reject) => {
+            const processOptions = {
+                cwd: process.cwd(),
+                stdio: 'inherit',
+                shell: true
+            };
 
-                promise.runnerInformation = {
-                    runnerIdentifier: `${scope.options.runnerAnnotation}${i}`,
-                    command: command
-                }
-                promise.then((result) => {
-                   handleRunnerLogs(result, promise);
-                }).catch((error)=>{
-                    handleRunnerLogs(error, promise);
-                    //we do not log anything here unless its possible to avoid this incorrect error report about the failed command
-                    //logger.error(`Iteration ${i} - Error: ${error.message}`);
-                });
-                runnerList.push(promise);
-            }
-    }
-    return runnerList;
-}
+            const child = spawn(executionDetails.command, processOptions);
+            child.on('exit', (exitCode) => {
+                resolve(timeMap);
+            });
+        });
 
-//WIP, not really well working right now!
-function handleRunnerLogs(result, promise) {
-    if (scope.options.runnerLog) {
-            if (!fs.existsSync(path.join(process.cwd(), scope.options.runnerLogFolderName)))
-                fs.mkdirSync(path.join(process.cwd(), scope.options.runnerLogFolderName));
-            const errorLogStream = fs.createWriteStream(`${scope.options.runnerLogFolderName}/${promise.runnerInformation.runnerIdentifier}-stderr.log`, {flags: 'a'});
-            const infoLogStream = fs.createWriteStream(`${scope.options.runnerLogFolderName}/${promise.runnerInformation.runnerIdentifier}-stdout.log`, {flags: 'a'});
-            try {
-                logger.log(`The Runner ${promise.runnerInformation.runnerIdentifier} is done!`);
-                errorLogStream.write(result.stderr);
-                infoLogStream.write(result.stdout);
-            } catch (error) {
-                logger.error(error);
-            } finally {
-                errorLogStream.close();
-                infoLogStream.close();
-            }
+        return promise;
     }
 }
